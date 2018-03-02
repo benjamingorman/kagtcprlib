e7a6afee649467acdda8b0aaaebbb6055d324d67
Benjamin Gorman <8076bgorman@gmail.com>
2018-03-02 19:43:37 +0000

    Add .nojekyll file to docs

--- make_docs.sh
+++ make_docs.sh
@@ -1,9 +1,10 @@
 export PYTHONPATH=$(pwd)
-mkdir -p docs docs_build
+mkdir -p docs\docs_build
 sphinx-apidoc --full --force -o docs_build/ kagtcprlib
 cp sphinx_config.py docs_build/
 cd docs_build/
 make singlehtml
 echo "Copying files output to docs/"
 cp -r _build/singlehtml/* ../docs
+touch ../docs/.nojekyll
 echo DONE
